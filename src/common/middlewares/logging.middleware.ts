07573ac55123569c237fe1257e3e637fcd606ba2
gustavodcdamas <gustavodcdamas@gmail.com>
2025-05-23 08:53:00 -0300

    add logging feature

---
+++ src/common/middlewares/logging.middleware.ts
@@ -0,0 +1,26 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { Logger } from '../../modules/logger/logger.service';
+
+@Injectable()
+export class LoggingMiddleware implements NestMiddleware {
+  constructor(private readonly logger: Logger) {}
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl, ip, headers } = req;
+    const userAgent = headers['user-agent'] || '';
+    const user = (req as any).user?.username || 'anonimo';
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const contentLength = res.get('content-length');
+
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip} - Usuário: ${user}`,
+        'HTTP'
+      );
+    });
+
+    next();
+  }
+}
---
+++ src/modules/logger/logger.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, Scope } from '@nestjs/common';
+import * as winston from 'winston';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
+
+@Injectable({ scope: Scope.TRANSIENT })
+export class Logger {
+  private logger: winston.Logger;
+
+  constructor() {
+    this.logger = winston.createLogger({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+      ),
+      transports: [
+        new DailyRotateFile({
+          filename: 'logs/application-%DATE%.log',
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '30d'
+        }),
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.simple()
+          )
+        })
+      ]
+    });
+  }
+
+  log(message: string, context?: string) {
+    this.logger.info(message, { context });
+  }
+
+  error(message: string, trace: string, context?: string) {
+    this.logger.error(message, { trace, context });
+  }
+
+  warn(message: string, context?: string) {
+    this.logger.warn(message, { context });
+  }
+
+  debug(message: string, context?: string) {
+    this.logger.debug(message, { context });
+  }
+}
